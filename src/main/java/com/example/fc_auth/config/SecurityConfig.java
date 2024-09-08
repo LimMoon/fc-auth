f6a9580c1abcb262bbd09aa1815c0515408ba7e4
LimMoon <lim.moon.95@gmail.com>
2024-09-08 11:38:07 +0900

    [5-2] 토큰 검증 로직 추가

--- src/main/java/com/example/fc_auth/config/SecurityConfig.java
+++ src/main/java/com/example/fc_auth/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.example.fc_auth.config;
 
 import com.example.fc_auth.filter.JwtAuthFilter;
+import com.example.fc_auth.repository.EmployeeRepository;
+import com.example.fc_auth.service.KakaoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -12,9 +15,14 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final KakaoService kakaoService;
+    private final EmployeeRepository employeeRepository;
+    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
+
     private static final String[] AUTH_ALLOWLIST = {
-            "/swagger-ui/**", "/v3/**"
+            "/swagger-ui/**", "/v3/**", "/login/**", "/images/**", "/kakao/**"
     };
 
     @Bean
@@ -25,10 +33,13 @@ public class SecurityConfig {
         http.sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.formLogin((form) -> form.disable());
 
-        http.addFilterBefore(new JwtAuthFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(new JwtAuthFilter(kakaoService, employeeRepository), UsernamePasswordAuthenticationFilter.class);
         http.authorizeHttpRequests(authorize -> authorize
                 .requestMatchers(AUTH_ALLOWLIST).permitAll()
                 .anyRequest().authenticated());
+
+        http.exceptionHandling((handling) -> handling.authenticationEntryPoint(authenticationEntryPoint));
+
         return http.build();
     }
 }
--- src/main/java/com/example/fc_auth/service/EmployeeService.java
+++ src/main/java/com/example/fc_auth/service/EmployeeService.java
@@ -3,17 +3,22 @@ package com.example.fc_auth.service;
 import com.example.fc_auth.model.Employee;
 import com.example.fc_auth.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmployeeService {
     private final EmployeeRepository employeeRepository;
 
     public List<Employee> listEmployees(){
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        log.info("Security Context Holder name: " + name);
         return employeeRepository.findAll();
     }
 

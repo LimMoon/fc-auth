21d8b03396e260ff7941f3088365b3f24ab8e568
LimMoon <lim.moon.95@gmail.com>
2024-10-15 15:08:42 +0900

    [7-2] threshold, throttling 기능

--- src/main/java/com/example/fc_auth/service/TokenService.java
+++ src/main/java/com/example/fc_auth/service/TokenService.java
@@ -1,5 +1,6 @@
 package com.example.fc_auth.service;
 
+import com.example.fc_auth.config.CustomRateLimiter;
 import com.example.fc_auth.dto.AppTokenRespDto;
 import com.example.fc_auth.dto.ValidateTokenDto;
 import com.example.fc_auth.model.Api;
@@ -11,6 +12,8 @@ import com.example.fc_auth.repository.DepartmentRepository;
 import com.example.fc_auth.util.JwtUtil;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -18,9 +21,11 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class TokenService {
     private final AppRepository appRepository;
     private final ApiRepository apiRepository;
+    private final CustomRateLimiter customRateLimiter;
 
     public AppTokenRespDto createAppToken(Long appId){
         App app = appRepository.getById(appId);
@@ -32,6 +37,14 @@ public class TokenService {
 
     public ResponseEntity<String> validateToken(ValidateTokenDto dto) {
         Api api = apiRepository.findByMethodAndPath(dto.getMethod(), dto.getPath());
-        return JwtUtil.validateAppToken(dto, api);
+        ResponseEntity resp = JwtUtil.validateAppToken(dto, api);
+        if(resp.getStatusCode().is2xxSuccessful()){
+            Long appId = Long.valueOf(JwtUtil.parseSubject(dto.getToken()));
+            if(!customRateLimiter.tryConsume(appId, api.getId())){
+                log.error("TOO MANY REQUESTS");
+                return new ResponseEntity<>("too many requests", HttpStatus.TOO_MANY_REQUESTS);
+            }
+        }
+        return resp;
     }
 }
